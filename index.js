aaec94cf013be5eaef5ecb6853324b88dea237d0
fengpan <fengpan90@yahoo.com>
2016-10-24 00:04:14 +0800

    add implode func

--- index.js
+++ index.js
@@ -425,6 +425,26 @@ Common.prototype.range = function(start, end, step) {
     return range;
 };
 
+Common.prototype.implode = function(glue, pieces) {
+    let i = '',
+        retVal = '',
+        tGlue = '';
+    if (arguments.length === 1) {
+        pieces = glue;
+        glue = '';
+    }
+    if (typeof pieces === 'object') {
+        if (Object.prototype.toString.call(pieces) === '[object Array]')
+            return pieces.join(glue);
+        for (i in pieces) {
+            retVal += tGlue + pieces[i];
+            tGlue = glue;
+        }
+        return retVal;
+    }
+    return pieces;
+};
+
 
 /**
  * @method      typeOf      明确诊断参数的具体类型
